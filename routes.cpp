3c5c59dabe9286efd79617e652796c7227cd6451
iamakash-06 <sathishakash2003@gmail.com>
2023-08-15 18:58:42 +0530

    CPP Pleae

--- routes.cpp
+++ routes.cpp
@@ -2,15 +2,41 @@
 
 crow::response greet()
 {
-    return crow::response{""};
+    return crow::response("Hello world!");
 }
+
 crow::response add(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+    if (!input)
+    {
+        return crow::response(400, "Both 'first' and 'second' numbers are required.");
+    }
+
+    int first = input["first"].i();
+    int second = input["second"].i();
+    int result = first + second;
+
+    crow::json::wvalue responseJson;
+    responseJson["result"] = result;
+
+    return crow::response(200, responseJson);
 }
+
 crow::response subtract(const crow::request &req)
 {
     auto input = crow::json::load(req.body);
-    return crow::response{""};
+    if (!input)
+    {
+        return crow::response(400, "Both 'first' and 'second' numbers are required.");
+    }
+
+    int first = input["first"].i();
+    int second = input["second"].i();
+    int result = first - second;
+
+    crow::json::wvalue responseJson;
+    responseJson["result"] = result;
+
+    return crow::response(200, responseJson);
 }
