259ab165b2ab761065afdde77bcc8d3006d5b354
iamakash-06 <sathishakash2003@gmail.com>
2023-08-15 22:22:17 +0530

    Runn

--- server.js
+++ server.js
@@ -2,37 +2,56 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+const baseUrl = '/calculator';
 
-const baseUrl = '/calculator'
+class Calculator {
+    static add(first, second) {
+        return first + second;
+    }
+
+    static subtract(first, second) {
+        return first - second;
+    }
+}
 
 app.use(express.json());
 
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('Hello World!');
+    return res.status(200).send('Hello World!');
 });
 
 baseRouter.post('/add', (req, res) => {
-    const {first, seocond} = req.body;
-    if (isNan(first) || isNaN(seocond)) {
-        return res.status(400).json({ error: "Both 'first' and 'second' numbers are required." });
+    const { first, second } = req.body;
+
+    if (typeof first !== 'number' || typeof second !== 'number') {
+        return res.status(400).json({ error: 'Both operands must be numbers' });
     }
-    const result = first + seocond;
-    res.status(200).json({result: result});
+
+    const result = Calculator.add(first, second);
+    res.status(200).json({ result });
 });
 
-
 baseRouter.post('/subtract', (req, res) => {
-    const {first, seocond} = req.body;
-    if (isNan(first) || isNaN(seocond)) {
-        return res.status(400).json({ error: "Both 'first' and 'second' numbers are required." });
+    const { first, second } = req.body;
+
+    if (typeof first !== 'number' || typeof second !== 'number') {
+        return res.status(400).json({ error: 'Both operands must be numbers' });
     }
-    const result = first - seocond;
-    res.status(200).json({result: result});
+
+    const result = Calculator.subtract(first, second);
+    res.status(200).json({ result });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Something went wrong!' });
 });
 
 app.use(baseUrl, baseRouter);
+
 app.listen(PORT, () => {
     console.log("Server running at PORT", PORT);
 });
